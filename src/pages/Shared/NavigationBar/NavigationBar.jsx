1de73e985811be52ea420cf387205bd963d0f73a
md-masum <masum.nextu@gmail.com>
2023-04-30 01:09:21 +0600

    hader nev style done

--- src/pages/Shared/NavigationBar/NavigationBar.jsx
+++ src/pages/Shared/NavigationBar/NavigationBar.jsx
@@ -20,7 +20,7 @@ const NavigationBar = () => {
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mx-auto">
-              <Link to="/category/0">Home</Link>
+              <Link className="d-flex align-items-center text-decoration-none text-secondary" to="/category/0">Home</Link>
               <Nav.Link href="#pricing">About</Nav.Link>
               <Nav.Link href="#pricing">Career</Nav.Link>
             </Nav>
